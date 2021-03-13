101aa1d613c97e10c0dcab105ad9d07c34edb813
Dank074 <torresefrain10@gmail.com>
2021-03-13 01:17:14 -0600

    added room outgoing session messages

---
+++ Turbo.Packets/Outgoing/Room/Session/RoomReadyMessage.cs
@@ -0,0 +1,10 @@
+﻿using Turbo.Core.Packets.Messages;
+
+namespace Turbo.Packets.Outgoing.Room.Session
+{
+    public record RoomReadyMessage : IComposer
+    {
+        public string RoomType { get; init; }
+        public int RoomId { get; init; }
+    }
+}
