dcba36584fece2ae08ba1e96c32acf8fbad5f750
Dank074 <torresefrain10@gmail.com>
2021-03-17 18:28:04 -0500

    fixed botskill packet

--- Turbo.Packets/Outgoing/Room/Bots/BotSkillListUpdateMessage.cs
+++ Turbo.Packets/Outgoing/Room/Bots/BotSkillListUpdateMessage.cs
@@ -1,10 +1,12 @@
-﻿using Turbo.Core.Packets.Messages;
+﻿using System.Collections.Generic;
+using Turbo.Core.Game.Rooms.Utils;
+using Turbo.Core.Packets.Messages;
 
 namespace Turbo.Packets.Outgoing.Room.Bots
 {
     public record BotSkillListUpdateMessage : IComposer
     {
         public int BotId { get; init; }
-        public int SkillList { get; init; }
+        public IList<BotSkillData> SkillList { get; init; }
     }
 }
