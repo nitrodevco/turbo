4b5e5634ff13e199786c3bb62449b91d38b1f285
Bill <billsonnn@live.com>
2021-03-19 02:45:13 -0400

    Add enums

---
+++ Turbo.Core/Game/Rooms/Constants/RoomEnterErrorEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turbo.Core.Game.Rooms.Constants
+{
+    public enum RoomEnterErrorEnum
+    {
+        RoomFull = 1,
+        NoEntry = 2,
+        EnterQueue = 3,
+        Banned = 4
+    }
+}
---
+++ Turbo.Core/Game/Rooms/Constants/RoomSettingsErrorEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turbo.Core.Game.Rooms.Constants
+{
+    public enum RoomSettingsErrorEnum
+    {
+        InvalidPassword = 5,
+        InvalidName = 7,
+        BadName = 8,
+        BadDescription = 10,
+        BadTags = 11,
+        InvalidTags = 12,
+        ToManyTags = 13
+    }
+}
