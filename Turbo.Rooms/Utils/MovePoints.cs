e48bd17d269b69043805ef5dd34ef16441c77db4
Bill <billsonnn@live.com>
2021-03-20 23:42:17 -0400

    Change move points

--- Turbo.Rooms/Utils/MovePoints.cs
+++ Turbo.Rooms/Utils/MovePoints.cs
@@ -7,17 +7,17 @@ namespace Turbo.Rooms.Utils
     public class MovePoints
     {
         public static IList<IPoint> StandardPoints = new List<IPoint>(new IPoint[] { 
+            new Point(-1, 0),
             new Point(0, -1),
             new Point(1, 0),
-            new Point(0, 1),
-            new Point(-1, 0) 
+            new Point(0, 1) 
         });
 
         public static IList<IPoint> DiagonalPoints = new List<IPoint>(new IPoint[] {
-            new Point(1, -1),
+            new Point(-1, -1),
             new Point(-1, 1),
-            new Point(1, 1),
-            new Point(-1, -1) 
+            new Point(1, -1),
+            new Point(1, 1) 
         });
 
         public static IList<IPoint> MovingPoints = StandardPoints.Concat(DiagonalPoints).ToList();
