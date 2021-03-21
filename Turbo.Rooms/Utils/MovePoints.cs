06fd0c736ad304b399415dc5c0e76fae385b046a
Bill <billsonnn@live.com>
2021-03-21 01:07:49 -0400

    Fix MovePoints

--- Turbo.Rooms/Utils/MovePoints.cs
+++ Turbo.Rooms/Utils/MovePoints.cs
@@ -7,14 +7,14 @@ namespace Turbo.Rooms.Utils
 {
     public class MovePoints
     {
-        public static IList<IPoint> StandardPoints = new List<IPoint>(new IPoint[] { 
+        public static IReadOnlyCollection<IPoint> StandardPoints = new List<IPoint>(new IPoint[] { 
             new Point(-1, 0),
             new Point(0, -1),
             new Point(1, 0),
             new Point(0, 1) 
         });
 
-        public static IList<IPoint> DiagonalPoints = new List<IPoint>(new IPoint[] {
+        public static IReadOnlyCollection<IPoint> DiagonalPoints = new List<IPoint>(new IPoint[] {
             new Point(-1, -1),
             new Point(-1, 1),
             new Point(1, -1),
