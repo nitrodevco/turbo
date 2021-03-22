b757caddca58431de5aa3d8e800ab1fd40514a46
Remco <habboilany@gmail.com>
2021-03-22 20:44:44 +0100

    Added unittests

--- Turbo.Main.Tests/PacketHandlers/NavigatorMessageHandlerTests.cs
+++ Turbo.Main.Tests/PacketHandlers/NavigatorMessageHandlerTests.cs
@@ -1,81 +1,110 @@
-﻿//using DotNetty.Transport.Channels;
-//using Microsoft.Extensions.Logging.Abstractions;
-//using Moq;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Turbo.Core.Game.Navigator;
-//using Turbo.Core.Game.Players;
-//using Turbo.Core.Networking.Game.Clients;
-//using Turbo.Core.PacketHandlers;
-//using Turbo.Core.Packets;
-//using Turbo.Core.Packets.Messages;
-//using Turbo.Core.Packets.Revisions;
-//using Turbo.Main.PacketHandlers;
-//using Turbo.Networking.Game.Clients;
-//using Turbo.Packets;
-//using Turbo.Packets.Incoming.Navigator;
-//using Turbo.Players;
-//using Xunit;
+﻿using AutoFixture;
+using DotNetty.Transport.Channels;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Turbo.Core.Game.Navigator;
+using Turbo.Core.Game.Players;
+using Turbo.Core.Networking.Game.Clients;
+using Turbo.Core.PacketHandlers;
+using Turbo.Core.Packets;
+using Turbo.Core.Packets.Revisions;
+using Turbo.Main.PacketHandlers;
+using Turbo.Networking.Game.Clients;
+using Turbo.Packets;
+using Turbo.Packets.Incoming.Navigator;
+using Turbo.Players;
+using Xunit;
 
-//namespace Turbo.Main.Tests.PacketHandlers
-//{
-//    public class NavigatorMessageHandlerTests
-//    {
-//        private readonly Mock<IPacketMessageHub> _messageHubMock;
-//        private readonly Mock<INavigatorManager> _navigatorManagerMock;
+namespace Turbo.Main.Tests.PacketHandlers
+{
+    public class NavigatorMessageHandlerTests
+    {
+        private readonly IPacketMessageHub _messageHub;
+        private readonly Mock<INavigatorManager> _navigatorManagerMock;
 
-//        private readonly ISession _session;
-//        private readonly IPlayer _player;
+        private readonly ISession _session;
+        private readonly IPlayer _player;
 
-//        private readonly INavigatorMessageHandler _sut;
+        private readonly IFixture _fixture;
 
-//        public NavigatorMessageHandlerTests()
-//        {
-//            _messageHubMock = new Mock<IPacketMessageHub>();
-//            _navigatorManagerMock = new Mock<INavigatorManager>();
+        private readonly INavigatorMessageHandler _sut;
 
-//            var channelHandlerContextMock = new Mock<IChannelHandlerContext>();
-//            var revisionMock = new Mock<IRevision>();
-//            _session = new Session(channelHandlerContextMock.Object, revisionMock.Object, new NullLogger<Session>());
+        public NavigatorMessageHandlerTests()
+        {
+            // Hub
+            _messageHub = new PacketMessageHub(new NullLogger<PacketMessageHub>());
+            _navigatorManagerMock = new Mock<INavigatorManager>();
 
-//            var playerContainerMock = new Mock<IPlayerContainer>();
-//            _player = new Player(playerContainerMock.Object, new NullLogger<IPlayer>(), new Database.Entities.Players.PlayerEntity());
+            // Session
+            var channelHandlerContextMock = new Mock<IChannelHandlerContext>();
+            var revisionMock = new Mock<IRevision>();
+            _session = new Session(channelHandlerContextMock.Object, revisionMock.Object, new NullLogger<Session>());
+            
+            // Player
+            var playerContainerMock = new Mock<IPlayerContainer>();
+            _player = new Player(playerContainerMock.Object, new NullLogger<IPlayer>(), new Database.Entities.Players.PlayerEntity());
 
-//            _sut = new NavigatorMessageHandler(_messageHubMock.Object, _navigatorManagerMock.Object);
-//        }
+            // Object mocking object
+            _fixture = new Fixture();
 
-//        [Fact]
-//        private void Constructor_SubscribesToMessages()
-//        {
-//            _messageHubMock.Verify(x => x.Subscribe<GetGuestRoomMessage>(_sut, _sut.))
-//        }
+            // System under test
+            _sut = new NavigatorMessageHandler(_messageHub, _navigatorManagerMock.Object);
+        }
 
-//        [Fact]
-//        private void OnNewNavigatorInit_WithPlayer_SendsPackets()
-//        {
-//            // Arrange
-//            _session.SetPlayer(_player);
+        [Fact]
+        private void Constructor_SubscribesToMessages()
+        {
+            Assert.True(_messageHub.Exists<GetGuestRoomMessage>(_sut));
+            Assert.True(_messageHub.Exists<NewNavigatorInitMessage>(_sut));
+        }
 
-//            // Act
-//            _messageHubMock.Publish(new NewNavigatorInitMessage(), _session);
+        [Fact]
+        private void OnNewNavigatorInit_WithPlayer_SendsPackets()
+        {
+            // Arrange
+            _session.SetPlayer(_player);
 
-//            // Assert
-//            _navigatorManagerMock.Verify(x => x.SendNavigatorMetaData(_session.Player), Times.Once);
-//            _navigatorManagerMock.Verify(x => x.SendNavigatorLiftedRooms(_session.Player), Times.Once);
-//        }
+            // Act
+            _messageHub.Publish(new NewNavigatorInitMessage(), _session);
 
-//        [Fact]
-//        private void OnNewNavigatorInit_WithoutPlayer_SendsPackets()
-//        {
-//            // Act
-//            _messageHub.Publish(new NewNavigatorInitMessage(), _session);
+            // Assert
+            _navigatorManagerMock.Verify(x => x.SendNavigatorMetaData(_session.Player), Times.Once);
+            _navigatorManagerMock.Verify(x => x.SendNavigatorLiftedRooms(_session.Player), Times.Once);
+        }
 
-//            // Assert
-//            _navigatorManagerMock.Verify(x => x.SendNavigatorMetaData(_session.Player), Times.Never);
-//            _navigatorManagerMock.Verify(x => x.SendNavigatorLiftedRooms(_session.Player), Times.Never);
-//        }
-//    }
-//}
+        [Fact]
+        private void OnNewNavigatorInit_WithoutPlayer_DoesNotSendPackets()
+        {
+            // Act
+            _messageHub.Publish(new NewNavigatorInitMessage(), _session);
+
+            // Assert
+            _navigatorManagerMock.Verify(x => x.SendNavigatorMetaData(_session.Player), Times.Never);
+            _navigatorManagerMock.Verify(x => x.SendNavigatorLiftedRooms(_session.Player), Times.Never);
+        }
+
+        [Fact]
+        private void GetGuestRoomMessage_WithPlayer_SendsPackets()
+        {
+            // Arrange
+            _session.SetPlayer(_player);
+            var message = _fixture.Create<GetGuestRoomMessage>();
+
+            // Act
+            _messageHub.Publish(message, _session);
+
+            // Assert
+            _navigatorManagerMock.Verify(x => x.GetGuestRoomMessage(_player, message.RoomId, message.EnterRoom, message.RoomForward), Times.Once);
+        }
+
+        [Fact]
+        private void GetGuestRoomMessage_WithoutPlayer_DoesNotSendPackets()
+        {
+            // Act
+            _messageHub.Publish(new NewNavigatorInitMessage(), _session);
+
+            // Assert
+            _navigatorManagerMock.Verify(x => x.GetGuestRoomMessage(It.IsAny<IPlayer>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<bool>()), Times.Never);
+        }
+    }
+}
