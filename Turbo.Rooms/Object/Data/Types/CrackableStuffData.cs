933c14bb92bab686813935d1ae1d6fa6b90745f1
Dank074 <torresefrain10@gmail.com>
2021-03-21 21:37:19 -0500

    started json stuffdata

--- Turbo.Rooms/Object/Data/Types/CrackableStuffData.cs
+++ Turbo.Rooms/Object/Data/Types/CrackableStuffData.cs
@@ -1,6 +1,13 @@
-﻿namespace Turbo.Rooms.Object.Data.Types
+﻿using System.IO;
+using System.Text.Json;
+
+namespace Turbo.Rooms.Object.Data.Types
 {
     public class CrackableStuffData : StuffDataBase
     {
+        public override void SerializeJson(Utf8JsonWriter writer, MemoryStream stream)
+        {
+            base.SerializeJson(writer, stream);
+        }
     }
 }
