6d767b908a530ff0524435b6084673cdd4688b69
Dank074 <torresefrain10@gmail.com>
2021-03-23 01:11:16 -0500

    im so fkn dumb

--- Turbo.Packets/Outgoing/Inventory/Furni/PostItPlacedMessage.cs
+++ Turbo.Packets/Outgoing/Inventory/Furni/PostItPlacedMessage.cs
@@ -1,6 +1,8 @@
-﻿namespace Turbo.Packets.Outgoing.Inventory.Furni
+﻿using Turbo.Core.Packets.Messages;
+
+namespace Turbo.Packets.Outgoing.Inventory.Furni
 {
-    public record PostItPlacedMessage
+    public record PostItPlacedMessage : IComposer
     {
         public int Id { get; init; }
         public int ItemsLeft { get; init; }
