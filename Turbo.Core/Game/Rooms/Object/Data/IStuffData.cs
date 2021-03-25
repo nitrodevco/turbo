4bf859c26c3f5c10c5a19bf614e177b9dce42ba3
Dank074 <torresefrain10@gmail.com>
2021-03-25 03:10:45 -0500

    add unique series getters to IStuffData

--- Turbo.Core/Game/Rooms/Object/Data/IStuffData.cs
+++ Turbo.Core/Game/Rooms/Object/Data/IStuffData.cs
@@ -5,7 +5,8 @@ namespace Turbo.Core.Game.Rooms.Object.Data
     public interface IStuffData
     {
         public int Flags { get; set; }
-
+        public int UniqueNumber { get; }
+        public int UniqueSeries { get; }
         public void WriteToPacket(IServerPacket packet);
         public string GetLegacyString();
         public void SetState(string state);
