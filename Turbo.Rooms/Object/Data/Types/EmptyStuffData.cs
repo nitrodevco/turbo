5282817b0b16451bffc9988481cddc45de0a321d
Bill <billsonnn@live.com>
2021-03-26 11:42:50 -0400

    Move stuff data WriteToPacket to plugin

--- Turbo.Rooms/Object/Data/Types/EmptyStuffData.cs
+++ Turbo.Rooms/Object/Data/Types/EmptyStuffData.cs
@@ -6,13 +6,6 @@ namespace Turbo.Rooms.Object.Data.Types
     {
         public string Data { get; set; }
 
-        public override void WriteToPacket(IServerPacket packet)
-        {
-            packet.WriteInteger(Flags);
-
-            base.WriteToPacket(packet);
-        }
-
         public override string GetLegacyString()
         {
             return Data == null ? "" : Data;
