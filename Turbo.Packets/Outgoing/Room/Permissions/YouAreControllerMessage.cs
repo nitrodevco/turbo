cc270946e614a4f4d0d9cb5031a7c336bd989c53
Dank074 <torresefrain10@gmail.com>
2021-03-29 22:36:55 -0500

    added room controller packets

---
+++ Turbo.Packets/Outgoing/Room/Permissions/YouAreControllerMessage.cs
@@ -0,0 +1,11 @@
+﻿using Turbo.Core.Game.Rooms.Constants;
+using Turbo.Core.Packets.Messages;
+
+namespace Turbo.Packets.Outgoing.Room.Permissions
+{
+    public record YouAreControllerMessage : IComposer
+    {
+        public int RoomId { get; set; }
+        public RoomControllerLevel RoomControllerLevel {get; set;}
+    }
+}
