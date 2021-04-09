4b2b50bb3234efe3bb30d3e1aa8b8fd5fba07029
Dank074 <torresefrain10@gmail.com>
2021-04-09 00:28:27 -0500

    forgot to implement interface

--- Turbo.Packets/Outgoing/Wired/WiredTriggerDataMessage.cs
+++ Turbo.Packets/Outgoing/Wired/WiredTriggerDataMessage.cs
@@ -1,8 +1,9 @@
 ﻿using Turbo.Core.Game.Rooms.Object;
+using Turbo.Core.Packets.Messages;
 
 namespace Turbo.Packets.Outgoing.Wired
 {
-    public record WiredTriggerDataMessage
+    public record WiredTriggerDataMessage : IComposer
     {
         public IRoomObject WiredTriggerObject { get; init; }
     }
