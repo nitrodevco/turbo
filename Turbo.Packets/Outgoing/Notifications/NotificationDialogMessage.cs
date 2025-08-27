f3e4ffa88632de90b4a202bc330f24b8d230a279
Stankman <rodolfo_xdr@outlook.com>
2025-08-26 23:51:33 -0500

    Added Bubble Alerts

---
+++ Turbo.Packets/Outgoing/Notifications/NotificationDialogMessage.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Turbo.Core.Packets.Messages;
+
+namespace Turbo.Packets.Outgoing.Notifications;
+public class NotificationDialogMessage : IComposer
+{
+    public required string Type { get; init; }
+    public IDictionary<string, string>? Keys { get; init; }
+}
